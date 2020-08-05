b050300c152eb173aea9d7b4a6f97dfa3e599165
Austin Mordahl <austin.mordahL@utdallas.edu>
2020-08-05 16:20:27 +0000

    added 1000 programs

---
+++ 1000-programs/split_2/test23_false-unreach-call.c
@@ -0,0 +1,52 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int(void);
+extern _Bool __VERIFIER_nondet_bool(void);
+
+struct dummy {
+  int a, b;
+  int array[16];
+};
+
+struct dummy d1, d2;
+
+void init()
+{
+  d1.a = __VERIFIER_nondet_int();
+  d1.b = __VERIFIER_nondet_int();
+
+  d2.a = __VERIFIER_nondet_int();
+  d2.b = __VERIFIER_nondet_int();
+}
+
+struct dummy *get_dummy()
+{
+  return (__VERIFIER_nondet_bool() ? &d1 : &d2);
+}
+
+int check(struct dummy *s1, int i)
+{
+  return s1->array[i] == i;
+}
+
+int main()
+{
+  init();
+  struct dummy *pd1 = get_dummy(), *pd2 = get_dummy();
+  int i = __VERIFIER_nondet_int();
+  if (pd1 != 0 && pd1 == pd2 && i >= 0 && i < 10) {
+    pd2->array[i] = i;
+    int *pa = &pd1->array[i];
+    i = pd2->array[i] - 10;
+    while (i <= *pa) {
+      ++i;
+    }
+    if (!check(pd2, i)) {
+      goto ERROR;
+    }
+  }
+
+  return 0;
+
+  ERROR: __VERIFIER_error();
+  return 1;
+}
---
+++ 1000-programs/split_6/test24_false-unreach-call.c
@@ -0,0 +1,39 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int();
+
+struct dummy {
+  int a, b;
+};
+
+int check(struct dummy *ad1, int b)
+{
+  return ad1[b].a == b;
+}
+
+int main()
+{
+  struct dummy ad1[20], *ad2;
+  int i, j, *pa;
+  for(j=0; j<20; j++) {
+    ad1[j].a = __VERIFIER_nondet_int();
+    ad1[j].b = __VERIFIER_nondet_int();
+  }
+  i = __VERIFIER_nondet_int();
+  if (i >= 0 && i < 10) {
+    ad2 = ad1;
+    ad1[i].a = i;
+    pa = &ad1[i].a;
+    i =  ad2[i].a + 10;
+    while (i < *pa) {
+      ++i;
+    }
+    if (!check(ad1, i)) {
+      goto ERROR;
+    }
+  }
+
+  return 0;
+
+  ERROR: __VERIFIER_error();
+  return 1;
+}
